6cd5bc39fdbfd0d21ce3df3d4e6e80a341b58477
JorgeCeb <franperceb@gmail.com>
2021-09-27 23:59:33 -0500

    Creando rama para trabajar en el proyecto

---
+++ AL/lexer/tokens.H
@@ -0,0 +1,21 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+const int INT = 1;
+const int FLOAT = 2;
+const int ELSE = 3;
+const int IF = 4;
+const int WHILE = 5;
+const int MAS = 6;
+const int MENOS = 7;
+const int MUL = 8;
+const int DIV = 9;
+const int ASIG = 10;
+const int LPAR = 11;
+const int RPAR = 12;
+const int ID = 13;
+const int PYC = 14;
+const int COMA = 15;
+const int NUMERO = 16;
+const int ESP = 17;
+#endif
